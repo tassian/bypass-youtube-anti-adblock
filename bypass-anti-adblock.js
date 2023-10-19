58a2db79cb69432d4ec87e50356150987801245e
Florian <80549556+tassian@users.noreply.github.com>
2023-10-20 00:57:46 +0200

    Update bypass-anti-adblock.js

--- bypass-anti-adblock.js
+++ bypass-anti-adblock.js
@@ -6,8 +6,8 @@
 // @author       tassian
 // @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
-// @updateURL    https://github.com/tassian/bypass-youtube-anti-adblock/raw/main/bypass-anti-adblock.js
-// @downloadURL  https://github.com/tassian/bypass-youtube-anti-adblock/raw/main/bypass-anti-adblock.js
+// @updateURL    https://github.com/tassian/bypass-youtube-anti-adblock/raw/main/bypass-anti-adblock.user.js
+// @downloadURL  https://github.com/tassian/bypass-youtube-anti-adblock/raw/main/bypass-anti-adblock.user.js
 // @grant        none
 // ==/UserScript==
 
